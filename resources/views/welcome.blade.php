9ed8e592efa4bb20856f33eadc8a4bd8c475fb9f
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2023-01-06 14:56:43 +0530

    Vite Changes for all Blade File Refresh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -409,7 +409,7 @@
                 @endauth
             </div>
         @endif
-        <div class="bg-red-600">
+        <div class="bg-blue-600">
             Langauge : <select onchange="changeLanguage(this.value)">
                 <option {{ session()->has('lang_code') ? (session()->get('lang_code') == 'en' ? 'selected' : '') : '' }}
                     value="en">English</option>
